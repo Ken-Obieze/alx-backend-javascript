d79de0bbe0d32d8bde9bbbab5599fc241a94ea8e
Ken-Obieze <kenneth.obieze@outlook.com>
2023-04-26 01:36:46 +0100

    tasks 8 - 100

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,41 +1,14 @@
 class HolbertonClass {
-  constructor(size, location) {
-    if (typeof size !== "number") {
-      throw new TypeError("size must be a number");
-    }
-    if (typeof location !== "string") {
-      throw new TypeError("location must be a string");
-    }
+  constructor(size = 0, location = '') {
     this._size = size;
     this._location = location;
   }
 
-  get size() {
-    return this._size;
-  }
-  set size(size) {
-    if (typeof size !== "number") {
-      throw new TypeError("size must be a number");
+  [Symbol.toPrimitive](atr) {
+    if (atr === 'number') {
+      return `${this._size}`;
     }
-    this._size = size;
-  }
-
-  get location() {
-    return this._location;
-  }
-  set location(location) {
-    if (typeof location !== "string") {
-      throw new TypeError("location must be a string");
-    }
-    this._location = location;
-  }
-
-  valueOf() {
-    return this._size;
-  }
-
-  toString() {
-    return this._location;
+    return `${this._location}`;
   }
 }
-
+export default HolbertonClass;
