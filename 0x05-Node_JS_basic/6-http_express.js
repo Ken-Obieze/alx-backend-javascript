9864920af5c7a02fb57787172bbfd80f0080ee73
Ken-Obieze <kenneth.obieze@outlook.com>
2023-06-14 02:27:37 +0000

    bulk commit

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,31 +1,12 @@
-const http = require('http');
-const countStudents = require('./3-read_file_async');
+const express = require('express');
 
-const app = http.createServer((req, res) => {
-  const { method, url } = req;
+const app = express();
+const port = 1245;
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-
-  if (method === 'GET') {
-    if (url === '/') {
-      res.end('Hello Holberton School!\n');
-    } else if (url === '/students') {
-      countStudents(process.argv[2])
-        .then((data) => {
-          res.end(`This is the list of our students\n${data}`);
-        })
-        .catch((error) => {
-          res.end(`This is the list of our students\n${error}`);
-        });
-    } else {
-      res.end('Not found\n');
-    }
-  } else {
-    res.end('Invalid request\n');
-  }
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
 
-app.listen(1245);
+app.listen(port);
 
 module.exports = app;
---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const database = req.query.database || 'no_database.csv';
+    const students = await countStudents(database);
+    res.send(`This is the list of our students\n${students}`);
+  } catch (error) {
+    res.send(`Error: ${error.message}`);
+  }
+});
+
+app.listen(port);
+
+module.exports = app;
