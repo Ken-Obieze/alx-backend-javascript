4ec8ed0896d528f73b5d268133d25af94b4050c2
Ken-Obieze <kenneth.obieze@outlook.com>
2023-06-17 05:43:27 +0100

    task 1 fix

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -6,11 +6,23 @@ describe('calculateNumber', () => {
     it('should round the numbers and return the sum', () => {
       assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
     });
+    it('equal positive numbers (alternate)', () => {
+      assert.strictEqual(calculateNumber('SUM', 2.3, 1.8), 4);
+    });
+    it('equal negative numbers', () => {
+      assert.strictEqual(calculateNumber('SUM', -2.0, -2.0), -4);
+    });
   });
 
   describe('type: SUBTRACT', () => {
     it('should round the numbers and return the difference', () => {
       assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+     it('equal negative numbers', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', -2.0, -2.0), 0);
+    });
+    it('equal negative numbers (alternate)', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', -2.3, -1.8), 0);
     });
   });
 
@@ -18,9 +30,14 @@ describe('calculateNumber', () => {
     it('should round the numbers and return the quotient', () => {
       assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
     });
-
     it('should return "Error" when the rounded value of b is 0', () => {
       assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+    it('numbers with different signs (alternate)', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 7.0, -2.0), -3.5);
+    });
+    it('negative numbers', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', -7.0, -2.0), 3.5);
     });
   });
 });
