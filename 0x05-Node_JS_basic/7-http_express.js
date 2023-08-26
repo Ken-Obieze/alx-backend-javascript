28d1e80c6ff14e49cd29123ae4612d0f44238aa1
Ken-Obieze <kenneth.obieze@outlook.com>
2023-08-26 05:49:58 +0100

    task 7 fix

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,25 +1,45 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
-const port = 1245;
+const databaseFileName = process.argv[2];
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!\n');
 });
 
-app.get('/students', async (req, res) => {
-  try {
-    const database = req.query.database || 'no_database.csv';
-    const students = await countStudents(database);
-    res.send(`This is the list of our students\n${students}`);
-  } catch (error) {
-    res.send(`Error: ${error.message}`);
-  }
+app.get('/students', (req, res) => {
+  const filePath = path.join(__dirname, databaseFileName);
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading the database file');
+      return;
+    }
+
+    const lines = data.trim().split('\n');
+    const students = lines.map(line => line.split(',')); // Parse CSV lines
+
+    const csStudents = students.filter(student => student[3].trim() === 'CS');
+    const sweStudents = students.filter(student => student[3].trim() === 'SWE');
+
+    const csStudentList = csStudents.map(student => student[0]).join(', ');
+    const sweStudentList = sweStudents.map(student => student[0]).join(', ');
+
+    const response = `
+      This is the list of our students
+      Number of students: ${students.length}
+      Number of students in CS: ${csStudents.length}. List: ${csStudentList}
+      Number of students in SWE: ${sweStudents.length}. List: ${sweStudentList}
+    `;
+
+    res.send(response);
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.listen(1245, () => {
+  console.log('Express server is listening on port 1245');
 });
 
 module.exports = app;
