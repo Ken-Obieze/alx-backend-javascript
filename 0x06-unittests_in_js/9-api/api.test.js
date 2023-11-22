7020ee8bca0e34ec44c661a5ea631d72c6e07115
Ken-Obieze <kenneth.obieze@outlook.com>
2023-11-22 03:58:05 +0100

    repo refactor

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -16,3 +16,30 @@ describe('Cart page', () => {
     });
   });
 });
+
+describe('GET /', () => {
+  it('returns "Welcome to the payment system"', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('GET /cart/:id', () => {
+  it('returns "Payment methods for cart :id"', (done) => {
+    request('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('returns 404 Not Found for non-numeric id', (done) => {
+    request('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
